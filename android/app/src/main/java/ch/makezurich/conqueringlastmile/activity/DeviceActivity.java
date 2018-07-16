c1ccd289fd0995fedd264e4dfe85a26d7fe01c4b
bytecoders <josepantoni.torres@gmail.com>
2018-07-16 20:15:55 +0200

    Refactoring of the picture functionalit, added also image rotation

--- android/app/src/main/java/ch/makezurich/conqueringlastmile/activity/DeviceActivity.java
+++ android/app/src/main/java/ch/makezurich/conqueringlastmile/activity/DeviceActivity.java
@@ -4,20 +4,15 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.net.Uri;
 import android.os.Bundle;
-import android.os.Environment;
-import android.provider.MediaStore;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.content.ContextCompat;
-import android.support.v4.content.FileProvider;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AlertDialog;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v7.graphics.Palette;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
@@ -27,8 +22,6 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
-import java.io.File;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -39,7 +32,7 @@ import ch.makezurich.conqueringlastmile.datastorage.DeviceProfile;
 import ch.makezurich.conqueringlastmile.fragment.FrameFragment;
 import ch.makezurich.ttnandroidapi.datastorage.api.Frame;
 
-public class DeviceActivity extends AppCompatActivity implements FrameFragment.OnFrameListFragmentInteractionListener {
+public class DeviceActivity extends PhotoActivity implements FrameFragment.OnFrameListFragmentInteractionListener {
     private static final String TAG = DeviceActivity.class.getSimpleName();
 
     private TTNApplication ttnApp;
@@ -49,8 +42,6 @@ public class DeviceActivity extends AppCompatActivity implements FrameFragment.O
     private ImageView ivTabHeader;
 
     public static final String EXTRA_DEVICE_ID = "EXTRA_DEVICE_ID";
-    private static final int REQUEST_IMAGE_CAPTURE = 1;
-    private String mCurrentPhotoPath;
 
     // Height and with for the device pictures
     private static final int DEVICE_PICTURE_WIDTH = 1280;
@@ -170,7 +161,7 @@ public class DeviceActivity extends AppCompatActivity implements FrameFragment.O
                 finish();
                 return true;
             case R.id.action_change_picture:
-                dispatchTakePictureIntent();
+                dispatchTakePictureIntent(DEVICE_PICTURE_WIDTH, DEVICE_PICTURE_HEIGHT);
                 return true;
             case R.id.action_change_name:
                 showChangeNameDialog();
@@ -262,71 +253,11 @@ public class DeviceActivity extends AppCompatActivity implements FrameFragment.O
         }
     }
 
-    private void dispatchTakePictureIntent() {
-        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        // Ensure that there's a camera activity to handle the intent
-        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-            // Create the File where the photo should go
-            File photoFile = null;
-            try {
-                photoFile = createImageFile();
-            } catch (IOException ex) {
-                // Error occurred while creating the File
-            }
-            // Continue only if the File was successfully created
-            if (photoFile != null) {
-                Uri photoURI = FileProvider.getUriForFile(this,
-                        "ch.makezurich.fileprovider",
-                        photoFile);
-                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
-                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
-            }
-        }
-    }
-
-    private File createImageFile() throws IOException {
-        // Create an image file name
-        String imageFileName = "tmp";
-        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
-        File image = File.createTempFile(
-                imageFileName,  /* prefix */
-                ".jpg",         /* suffix */
-                storageDir      /* directory */
-        );
-
-        // Save a file: path for use with ACTION_VIEW intents
-        mCurrentPhotoPath = image.getAbsolutePath();
-        return image;
-    }
-
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-            final Bitmap picture = adjustPicture(DEVICE_PICTURE_WIDTH, DEVICE_PICTURE_HEIGHT);
-            deviceProfile.setPicture(picture);
-            ivTabHeader.setImageBitmap(picture);
-            setupPalette(picture);
-            savedata();
-            new File(mCurrentPhotoPath).delete();
-        }
-    }
-
-    private Bitmap adjustPicture(int targetW, int targetH) {
-        // Get the dimensions of the bitmap
-        BitmapFactory.Options bmOptions = new BitmapFactory.Options();
-        bmOptions.inJustDecodeBounds = true;
-        BitmapFactory.decodeFile(mCurrentPhotoPath, bmOptions);
-        int photoW = bmOptions.outWidth;
-        int photoH = bmOptions.outHeight;
-
-        // Determine how much to scale down the image
-        int scaleFactor = Math.min(photoW/targetW, photoH/targetH);
-
-        // Decode the image file into a Bitmap sized to fill the View
-        bmOptions.inJustDecodeBounds = false;
-        bmOptions.inSampleSize = scaleFactor;
-        bmOptions.inPurgeable = true;
-
-        return BitmapFactory.decodeFile(mCurrentPhotoPath, bmOptions);
+    protected void onPictureTaken(Bitmap picture) {
+        deviceProfile.setPicture(picture);
+        ivTabHeader.setImageBitmap(picture);
+        setupPalette(picture);
+        savedata();
     }
 }
---
+++ android/app/src/main/java/ch/makezurich/conqueringlastmile/activity/PhotoActivity.java
@@ -0,0 +1,136 @@
+package ch.makezurich.conqueringlastmile.activity;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.media.ExifInterface;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v4.content.FileProvider;
+import android.support.v7.app.AppCompatActivity;
+
+import java.io.File;
+import java.io.IOException;
+
+public abstract class PhotoActivity extends AppCompatActivity {
+    private static final int REQUEST_IMAGE_CAPTURE = 1;
+    private String mCurrentPhotoPath;
+    private int pictureWidth;
+    private int pictureHeight;
+
+    protected void dispatchTakePictureIntent(int pictureWidth, int pictureHeight) {
+        this.pictureWidth = pictureWidth;
+        this.pictureHeight = pictureHeight;
+
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this,
+                        "ch.makezurich.fileprovider",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+            }
+        }
+    }
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String imageFileName = "tmp";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        mCurrentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bitmap picture = resizePicture();
+            picture = checkOrientation(picture);
+            if (picture != null) {
+                onPictureTaken(picture);
+            }
+            new File(mCurrentPhotoPath).delete();
+        }
+    }
+
+    private Bitmap checkOrientation(Bitmap bitmap) {
+        try {
+            ExifInterface ei = new ExifInterface(mCurrentPhotoPath);
+            int orientation = ei.getAttributeInt(ExifInterface.TAG_ORIENTATION,
+                    ExifInterface.ORIENTATION_UNDEFINED);
+
+            Bitmap rotatedBitmap = null;
+            switch (orientation) {
+
+                case ExifInterface.ORIENTATION_ROTATE_90:
+                    rotatedBitmap = rotateImage(bitmap, 90);
+                    break;
+
+                case ExifInterface.ORIENTATION_ROTATE_180:
+                    rotatedBitmap = rotateImage(bitmap, 180);
+                    break;
+
+                case ExifInterface.ORIENTATION_ROTATE_270:
+                    rotatedBitmap = rotateImage(bitmap, 270);
+                    break;
+
+                case ExifInterface.ORIENTATION_NORMAL:
+                default:
+                    rotatedBitmap = bitmap;
+            }
+
+            return rotatedBitmap;
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
+
+        return bitmap;
+    }
+
+    private Bitmap resizePicture() {
+        // Get the dimensions of the bitmap
+        BitmapFactory.Options bmOptions = new BitmapFactory.Options();
+        bmOptions.inJustDecodeBounds = true;
+        BitmapFactory.decodeFile(mCurrentPhotoPath, bmOptions);
+        int photoW = bmOptions.outWidth;
+        int photoH = bmOptions.outHeight;
+
+        // Determine how much to scale down the image
+        int scaleFactor = Math.min(photoW/pictureWidth, photoH/pictureHeight);
+
+        // Decode the image file into a Bitmap sized to fill the View
+        bmOptions.inJustDecodeBounds = false;
+        bmOptions.inSampleSize = scaleFactor;
+        bmOptions.inPurgeable = true;
+
+        return BitmapFactory.decodeFile(mCurrentPhotoPath, bmOptions);
+    }
+
+    private static Bitmap rotateImage(Bitmap source, float angle) {
+        Matrix matrix = new Matrix();
+        matrix.postRotate(angle);
+        return Bitmap.createBitmap(source, 0, 0, source.getWidth(), source.getHeight(),
+                matrix, true);
+    }
+
+    protected abstract void onPictureTaken(Bitmap picture);
+}
